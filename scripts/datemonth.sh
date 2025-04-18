6d7733f2823c24abb5428303980f05f2f0473413
NickyNguyen1910 <nguyenmanhhung19102003@gmail.com>
2025-04-18 23:29:19 +0700

    Update datemonth.sh

--- scripts/datemonth.sh
+++ scripts/datemonth.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-YYYY = $(date +%Y)
-MM = $(date +%m)    
-DD = $(date +%d)
+YYYY=$(date +%Y)
+MM=$(date +%m)    
+DD=$(date +%d)
 
 aws s3 cp --recursive CICD.tar s3://deu1-universal-bucket/hung.nm/code/YYYY/MM/DD/CICD.tar.gz
 
