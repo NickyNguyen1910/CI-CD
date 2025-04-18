eb82ccdb98c43e3b70bf6e6781f9cdda078e0bfe
NickyNguyen1910 <nguyenmanhhung19102003@gmail.com>
2025-04-18 23:22:22 +0700

    Update datemonth.sh

--- scripts/datemonth.sh
+++ scripts/datemonth.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "Today is $(date +%Y-%m-%d)"
+YYYY = $(date +%Y)
+MM = $(date +%m)    
+DD = $(date +%d)
+
+cp CICD.tar deu1-universal-bucket/nam.dh/code/YYYY/MM/DD/CICD.tar.gz
+
+
